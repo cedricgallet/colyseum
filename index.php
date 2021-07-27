fae298de232b40236c20d9a73c7a84004d1e8150
cedricgallet <galletcedric@protonmail.com>
2021-07-27 12:31:07 +0200

    mise en place mvc

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Génération du Navbar:
+include(dirname(__FILE__).'/views/templates/navBar.php');
+
+// Génération du Navbar:
+include(dirname(__FILE__).'/controllers/connexion.php');
+
+
+// Génération du Footer:
+include(dirname(__FILE__).'/views/templates/footer.php');
+
+
+
+
+
